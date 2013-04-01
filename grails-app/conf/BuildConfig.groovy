72799f269bba696162d22c78190ec04a75d5cbc9
Burt Beckwith <burt@burtbeckwith.com>
2013-04-01 15:08:03 -0400

    updated Netty to 4.0.0.CR1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,36 +12,36 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		String nettyVersion = '4.0.0.Beta2'
+		String nettyVersion = '4.0.0.CR1'
 
 		compile "io.netty:netty-buffer:$nettyVersion", {
-			excludes 'easymock', 'easymockclassextension', 'hamcrest-library', 'javassist', 'jmock-junit4',
-			         'junit', 'logback-classic', 'netty-common'
+			excludes 'easymock', 'easymockclassextension', 'javassist', 'jmock-junit4', 'junit',
+			         'logback-classic', 'netty-common'
 		}
 
 		compile "io.netty:netty-codec:$nettyVersion", {
-			excludes 'easymock', 'easymockclassextension', 'hamcrest-library', 'javassist',
-			         'jboss-marshalling', 'jboss-marshalling-river', 'jboss-marshalling-serial',
-			         'jmock-junit4', 'junit', 'jzlib', 'logback-classic', 'netty-transport', 'protobuf-java'
+			excludes 'easymock', 'easymockclassextension', 'javassist', 'jboss-marshalling',
+			         'jboss-marshalling-river', 'jboss-marshalling-serial', 'jmock-junit4',
+			         'junit', 'jzlib', 'logback-classic', 'netty-transport', 'protobuf-java'
+		}
+
+		compile "io.netty:netty-codec-http:$nettyVersion", {
+			excludes 'easymock', 'easymockclassextension', 'javassist', 'jmock-junit4', 'junit',
+			         'jzlib', 'logback-classic', 'netty-codec', 'netty-handler'
 		}
 
 		compile "io.netty:netty-common:$nettyVersion", {
-			excludes 'commons-logging', 'easymock', 'easymockclassextension', 'hamcrest-library', 'javassist',
+			excludes 'commons-logging', 'easymock', 'easymockclassextension', 'javassist',
 			         'jmock-junit4', 'junit', 'log4j', 'logback-classic', 'slf4j-api'
 		}
 
 		compile "io.netty:netty-handler:$nettyVersion", {
-			excludes 'easymock', 'easymockclassextension', 'hamcrest-library', 'javassist', 'jmock-junit4',
+			excludes 'easymock', 'easymockclassextension', 'javassist', 'jmock-junit4',
 			         'junit', 'logback-classic', 'netty-buffer', 'netty-transport'
 		}
 
-		compile "io.netty:netty-codec-http:$nettyVersion", {
-			excludes 'easymock', 'easymockclassextension', 'hamcrest-library', 'javassist', 'jmock-junit4',
-			         'junit', 'jzlib', 'logback-classic', 'netty-codec', 'netty-handler'
-		}
-
 		compile "io.netty:netty-transport:$nettyVersion", {
-			excludes 'easymock', 'easymockclassextension', 'hamcrest-library', 'javassist', 'jmock-junit4',
+			excludes 'easymock', 'easymockclassextension', 'javassist', 'jmock-junit4',
 			         'junit', 'logback-classic', 'netty-buffer'
 		}
 	}
